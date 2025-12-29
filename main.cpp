55a13c9b311845d06ce70c48b65285f8e663cf71
poallele <phillmathwayforum@gmail.com>
2025-12-29 12:25:37 -0500

    Refactor main.cpp to include comments and log output
    Added multi-line comment and logarithm calculation.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 int main()
 {
-    std::cout << "Here is some text.";
-    return 0;
+	/* This is a multi-line comment.
+	 * the matching asterisks to the left
+	 * can make this easier to read
+	 */
+
+	// direct initialization of a (integer) and b (integer)
+	int a(6), b(6);
+
+	// print (std::cout <<) the natural logarithm of (a multiplied by b)
+	std::cout << std::log(a * b);
+	return 0;
 }
