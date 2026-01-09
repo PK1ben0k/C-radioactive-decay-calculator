d31a5dd7490601e2f157346caf4c16269339d120
poallele <phillmathwayforum@gmail.com>
2026-01-09 16:43:41 -0500

    Refactor isotope selection and variable names
    Refactor user input handling for isotope selection and fix variable naming.

--- main.cpp
+++ main.cpp
@@ -18,11 +18,11 @@ int main()
      * multiplying them by the number of gregorian calendar
      * seconds. dividing it by 60 yields minutes. */
     bool exitLoop = false;
-    char responseOne{};
+    char response1{};
     while (!exitLoop) {
         std::cout << "Would you like to measure in years, seconds, or minutes? (y/s/m)";
-        std::cin >> responseOne;
-        switch (responseOne) {
+        std::cin >> response1;
+        switch (response1) {
             case 'y':
                 unit = "year";
                 multiplier = 1;
@@ -50,22 +50,29 @@ int main()
 	const double HALF_LIFE_232Th(1.40e10*multiplier);
 	
 	// user prompt
-	std::cout << "Select a fertile radioactive isotope:\n<1>Uranium-233\n<2>Uranium-235\n<3>Uranium-238\n<4>Plutonium-239\n<5>Thorium-232\n";
-    int responseTwo{};
-    std::cin >> responseTwo;
-    double isotope{};
-    if (responseTwo == 1) {
-        isotope = HALF_LIFE_233U;
-    } else if (responseTwo == 2) {
-        isotope = HALF_LIFE_235U;
-    } else if (responseTwo == 3) {
-        isotope = HALF_LIFE_238U;
-    } else if (responseTwo == 4) {
-        isotope = HALF_LIFE_239Pu;
-    } else if (responseTwo == 5) {
-        isotope = HALF_LIFE_232Th;
+	bool exitLoop2 = false;
+	char response2{};
+	double isotope{};
+	while (!exitLoop2) {
+        std::cout << "Select a fertile radioactive isotope:\n<1>Uranium-233\n<2>Uranium-235\n<3>Uranium-238\n<4>Plutonium-239\n<5>Thorium-232\n";
+	    std::cin >> response2;
+        switch (response2) {
+            case 1:
+                isotope = HALF_LIFE_233U;
+            case 2:
+                isotope = HALF_LIFE_235U;
+            case 3:
+                isotope = HALF_LIFE_238U;
+            case 4:
+                isotope = HALF_LIFE_239Pu;
+            case 5:
+                isotope = HALF_LIFE_232Th;
+            default:
+                std::cout << "Please enter a valid number.\n";
+                break;
+        }
     }
-    
+	
 	/* decay constant | the natural logarithm of  2 divided by the half life
 	 * print the decay constant as a number with 20 decimal places */
 	const double decay = (std::log(2)) / isotope;
